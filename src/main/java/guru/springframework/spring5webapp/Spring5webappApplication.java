b6d17784042bbc3aca74f6012b32c55d7585af89
Diego Ramirez Caballero <diego.fer659@gmail.com>
2021-09-02 19:44:00 -0500

    change implementation to use artemis mq docker

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,10 +1,5 @@
 package guru.springframework.spring5webapp;
 
-import org.apache.activemq.artemis.api.core.TransportConfiguration;
-import org.apache.activemq.artemis.core.config.Configuration;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,12 +8,13 @@ public class Spring5webappApplication {
 
     public static void main(String[] args) throws Exception {
 
-        Configuration config = new ConfigurationImpl();
+        //se comenta código debido que se utilizará un servidor local
+        /*Configuration config = new ConfigurationImpl();
         config.setPersistenceEnabled(false);
         config.setSecurityEnabled(false);
         config.setJournalDirectory("target/data/journal");
         config.getAcceptorConfigurations().add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
-        ActiveMQServers.newActiveMQServer(config);
+        ActiveMQServers.newActiveMQServer(config);*/
 
         SpringApplication.run(Spring5webappApplication.class, args);
     }
